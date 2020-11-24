28b62660fe4e9c1d8c002e8446cc832cbfa70cb2
Thomas Rager <tomrager@yahoo.com>
2020-11-24 12:01:25 -0500

    completed store.js and now working on api routes

--- db/store.js
+++ db/store.js
@@ -1,38 +1,52 @@
 //write and read file using fs module
-//create a class (writeNote, readNote, getNote and a few more) and export it
+const util=require ('util');
 const fs = require('fs');
+
 let num=0;
 
-class Store{
-    getNotes(){
-      fs.readFileSync('db.json',(data)=>{
-        return data;
-      })
-    }
+const readFileAsync=util.promisify(fs.readFile);
+const writeFileAsync=util.promisify(fs.writeFile);
 
-    addNote(note){
-      const {title,text}=note;
+class Store{
+
+    read(){
+      return readFileAsync('db/db.json', 'utf8');
+    }  
+
+    write(note){
+      return writeFileAsync('db/db.json', JSON.stringify(note));
+   }
+
+   getNotes() {
+    return this.read().then((notes) => {
+        let parsedNotes;
+        try {
+          parsedNotes = [].concat(JSON.parse(notes));
+        } catch (err) {
+          parsedNotes = [];
+        }
+      return parsedNotes;
+    });
+  }
+
+  addNote(note) {
+    const {title,text}=note;
     if (!title || !text){
-      throw new Error("Neither text or title can be blank");
+      throw new Error("Neither text or title can be blank for the note entered");
+    }
     const newNote={title,text,id:num};
     num++;
     this.getNotes()
       .then((notes)=>[...notes,newNote])
       .then((currentNotes)=>this.write(currentNotes))
       .then(()=>newNote);
-    }
-    
-    write(note){
-       id++;
-       fs.writeFileSync("./db.json", note);
-    }
+  }
 
-    removeNote(id){
+  removeNote(id) {
         return this.getNotes()
-        .then((notes)=>notes.filter(note)=>note.id !== id)
-        .then((filteredNotes)=>this.write(filteredNotes));
-    }
-    
+          .then((notes)=>notes.filter((note)=>note.id !== id))
+          .then((filteredNotes)=>this.write(filteredNotes));
+  } 
 }
 
 module.exports= new Store();
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,8 +3,7 @@
 // We are linking our routes to a series of "data" sources.
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
-
-const fs= require('fs');
+const { addNote, removeNote } = require('../db/store');
 const store= require('./db/store')
 let num=0;
 
@@ -15,38 +14,18 @@ let num=0;
 module.exports = function(app) {
   // API GET Requests
   app.get("/api/notes", function(req, res) {
-    fs.readFileSync('./db/db.json','utf8', (err, data) => {
-      if (err) throw err;
-        let parsedNotes;
-        try {
-          parsedNotes = [].concat(JSON.parse(data));
-        } catch (err) {
-          parsedNotes = [];
-        }
-    })  
+    getNotes();
   });
 
   // API POST Requests
   app.post("/api/notes", function(req, res) {
-    
-    }    
+    addNote(req.body);
   });
 
   //API DELETE Requests
   app.delete(req, res){
-    fs.readFileSync('./db/db.json','utf8', (err,data)=>{
-      if (err) throw err;
-      let parsedNotes;
-      try{
-        parsedNotes = [].concat(JSON.parse(data));
-      } catch (err) {
-        parsedNotes=[];
-      }
-      const notes=parsedNotes.filter(id);
-      }
-    })
+    removeNote();
   }
-
 }
 
   
